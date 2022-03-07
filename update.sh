0ca3fb98d79eb95db671334b72857f030f691344
Melzer, Finn <finn.melzer@wacker.com>
2022-03-07 10:36:53 +0100

    Fixed path

--- update.sh
+++ update.sh
@@ -3,13 +3,11 @@
 python3 -m venv $2
 . $2/bin/activate
 
-pip install -U pip wheel setuptools
-
 pip install -r $1
 pip freeze > $3
 # Remove pkg-resources as it can cause problems on dockerbuild
 #(see https://stackoverflow.com/questions/40670602/could-not-find-a-version-that-satisfies-the-requirement-pkg-resources-0-0-0)
-sed -i '/pkg-resources/d' ./mounted/requirements.txt
+sed -i '/pkg-resources/d' $3
 deactivate
 rm -r $2
 exit 0
